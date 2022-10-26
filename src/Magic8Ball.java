30f497fb5f0508a83619bbbc42e91d9839cb8955
1E10 Laptop <sample@bths.edu>
2022-10-26 10:29:29 -0400

    e

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Magic8Ball {
+import java.util.Scanner;
+public class Magic8Ball
+{
+    public static void main(String[] args)
+    {
+        SelectionMadness madness = new SelectionMadness();
+
+        // test flipcoin -- these will return true or false randomly
+        System.out.println(madness.flipCoin());
+        System.out.println(madness.flipCoin());
+        System.out.println(madness.flipCoin());
+
+        // test largest
+        System.out.println(madness.largest(5, 8, 3));
+        System.out.println(madness.largest(8, 5, 3));
+        System.out.println(madness.largest(3, 5, 8));
+        System.out.println(madness.largest(-5, -8, -3));
+        System.out.println(madness.largest(11, 11, 10));
+
+        // test rightTriangle
+        System.out.println(madness.rightTriangle(3, 4, 5));
+        System.out.println(madness.rightTriangle(5, 4, 3));
+        System.out.println(madness.rightTriangle(6, 8, 10));
+        System.out.println(madness.rightTriangle(8, 10, 6));
+        System.out.println(madness.rightTriangle(3, 6, 5));
+        System.out.println(madness.rightTriangle(1, 2, 3));
+
+    }
 }
--- src/SelectionMadness.java
+++ src/SelectionMadness.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class SelectionMadness {
+public class SelectionMadness {
+    public SelectionMadness() { }
+
+    public boolean flipCoin() {
+        int random = (int) (Math.random() * 2) + 1;
+        return random > 1;
+    }
+
+    public int largest(int num1, int num2, int num3) {
+        if (num1 >= num2) {
+            if (num1 >= num3) {
+                return num1;
+            }
+        }
+        if (num2 >= num1){
+            if (num2 >= num3){
+                return num2;
+            }
+        }
+        return num3;
+    }
+
+    public boolean rightTriangle(int side1, int side2, int side3) {
+        if (largest(side1, side2, side3) == side1){
+            if (Math.pow(side1,2) == Math.pow(side2,2) + Math.pow(side3, 2)){
+                return true;
+            }
+            return false;
+        }
+        if (largest(side1, side2, side3) == side2){
+            if (Math.pow(side2,2) == Math.pow(side1,2) + Math.pow(side3, 2)){
+                return true;
+            }
+            return false;
+        }
+        if (largest(side1, side2, side3) == side3){
+            if (Math.pow(side3,2) == Math.pow(side1,2) + Math.pow(side2, 2)){
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
 }
